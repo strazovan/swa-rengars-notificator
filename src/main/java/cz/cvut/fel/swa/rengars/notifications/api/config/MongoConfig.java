2e2f1a553759844ef0c5100dd943cb28cd20410d
David Stražovan <strazdav@fel.cvut.cz>
2021-06-03 09:23:59 +0200

    removes hardcoded database name

--- src/main/java/cz/cvut/fel/swa/rengars/notifications/api/config/MongoConfig.java
+++ src/main/java/cz/cvut/fel/swa/rengars/notifications/api/config/MongoConfig.java
@@ -30,7 +30,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return "notifications";
+        return this.mongoDatabase;
     }
 
     @Override
